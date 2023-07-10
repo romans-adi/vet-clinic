dd1f13144781195be4b0e3d14b025add7750e553
Romans S <romans.spilaks@gmail.com>
2023-07-10 23:53:31 +0300

    Add DB schema with auto_incrementing primary key (id) and associated columns name (varchar with char limit 25), date_of_birth (date), escape_attempts(int), neutered (bool), weight_kg(decimal, precision of 10 scale of 2). Drop DB if exists to reset all the data before creation exec

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+DROP TABLE IF EXISTS animals;
+
 CREATE TABLE animals (
-    name varchar(100)
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(25),
+  date_of_birth DATE,
+  escape_attempts INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL(10, 2)
 );
