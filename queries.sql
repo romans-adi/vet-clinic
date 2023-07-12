f5f138d709c6f19d1025f1c2d4f041db7be0b613
Romans S <romans.spilaks@gmail.com>
2023-07-13 02:56:32 +0300

    For the first query change name to animal_name to show more descriptive data info

--- queries.sql
+++ queries.sql
@@ -48,7 +48,7 @@ SELECT a.name FROM animals AS a JOIN species AS s ON a.species_id = s.id JOIN ow
 SELECT a.name FROM animals AS a JOIN owners AS o ON a.owner_id = o.id WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
 SELECT o.full_name, COUNT(a.id) AS animal_count FROM owners AS o LEFT JOIN animals AS a ON o.id = a.owner_id GROUP BY o.full_name HAVING COUNT(a.id) = (SELECT COUNT(id) FROM animals GROUP BY owner_id ORDER BY COUNT(id) DESC LIMIT 1);
 
-SELECT a.name FROM animals AS a JOIN visits AS v ON a.id = v.animal_id JOIN vets AS vt ON vt.id = v.vet_id WHERE vt.name = 'William Tatcher' ORDER BY v.visit_date DESC LIMIT 1;
+SELECT a.name AS animal_name FROM animals AS a JOIN visits AS v ON a.id = v.animal_id JOIN vets AS vt ON vt.id = v.vet_id WHERE vt.name = 'William Tatcher' ORDER BY v.visit_date DESC LIMIT 1;
 SELECT COUNT(DISTINCT a.id) AS animal_count FROM animals AS a JOIN visits AS v ON a.id = v.animal_id JOIN vets AS vt ON vt.id = v.vet_id WHERE vt.name = 'Stephanie Mendez';
 SELECT vt.name AS vet_name, s.species_id AS specialization_id FROM vets AS vt LEFT JOIN specializations AS s ON vt.id = s.vet_id ORDER BY vt.name;
 SELECT a.name AS animals_name FROM animals AS a JOIN visits AS v ON a.id = v.animal_id JOIN vets AS vt ON vt.id = v.vet_id WHERE vt.name = 'Stephanie Mendez' AND v.visit_date BETWEEN '2020-04-01' AND '2020-08-30';
